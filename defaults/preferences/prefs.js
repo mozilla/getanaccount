14938018d4e9b35e8a822e3f5694b7f7f7edd5d4
Blake Winton <bwinton@latte.ca>
2010-07-19 16:21:49 -0400

    Initial cut.

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.accountprovisioner.boolpref", false);
+pref("extensions.accountprovisioner.intpref", 0);
+pref("extensions.accountprovisioner.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.accountprovisioner@latte.ca.description", "chrome://accountprovisioner/locale/overlay.properties");
