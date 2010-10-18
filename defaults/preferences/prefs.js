588c61e373f49c7ec42ef75df5d21fc92caeaed1
Blake Winton <bwinton@latte.ca>
2010-10-18 15:40:04 -0400

    Move one of the services over to our back-end.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.accountprovisioner@latte.ca.description", "chrome://accountprovisioner/locale/overlay.properties");
-pref("extensions.accountprovisioner.suggestFromName", "http://bwinton.latte.ca/work/provision/suggestFromName.cgi");
+pref("extensions.accountprovisioner.suggestFromName", "http://momoprov.last-chance.net/suggest");
 pref("extensions.accountprovisioner.checkAddress", "http://bwinton.latte.ca/work/provision/checkAddress.cgi");
 pref("extensions.accountprovisioner.provision", "http://bwinton.latte.ca/work/provision/provision.cgi");
 
