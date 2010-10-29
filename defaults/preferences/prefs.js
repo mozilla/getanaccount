8c2635e6b4759704ae24d48ecab8afddbcde5e49
Blake Winton <bwinton@latte.ca>
2010-10-29 11:44:24 -0400

    Switch over to 0.9, and the live momo servers.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.accountprovisioner@latte.ca.description", "chrome://accountprovisioner/locale/overlay.properties");
-pref("extensions.accountprovisioner.providerList", "http://momoprov.last-chance.net/providers");
-pref("extensions.accountprovisioner.suggestFromName", "http://momoprov.last-chance.net/suggest");
-pref("extensions.accountprovisioner.checkAddress", "http://bwinton.latte.ca/work/provision/checkAddress.cgi");
+pref("extensions.accountprovisioner.providerList", "http://live.mozillamessaging.com/provider/list");
+pref("extensions.accountprovisioner.suggestFromName", "http://live.mozillamessaging.com/provider/hover/suggest");
+pref("extensions.accountprovisioner.checkAddress", "http://live.mozillamessaging.com/provider/hover/check");
 
