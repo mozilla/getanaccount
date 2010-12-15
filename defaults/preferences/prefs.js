a1d8bf1893d601607c422dab3a3c1f88805dec7e
Blake Winton <bwinton@latte.ca>
2010-12-14 20:16:58 -0500

    Fix the email_providers url, and use mozillamessaging.com instead of latte.ca.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.getanaccount@latte.ca.description", "chrome://getanaccount/locale/overlay.properties");
+pref("extensions.getanaccount@mozillamessaging.com.description", "chrome://getanaccount/locale/overlay.properties");
 pref("extensions.getanaccount.providerList", "http://broker.live.mozillamessaging.com/provider/list");
 pref("extensions.getanaccount.logUrl", "http://broker.live.mozillamessaging.com/provider/log");
 pref("extensions.getanaccount.suggestFromName", "http://broker.live.mozillamessaging.com/provider/hover/suggest");
