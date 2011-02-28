f35c75f8deb931f31fc8757483915aabff31ce78
Blake Winton <bwinton@latte.ca>
2011-02-28 13:12:09 -0500

    Switch over to https to prevent MITM attacks.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,7 +1,7 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.getanaccount@mozillamessaging.com.description", "chrome://getanaccount/locale/overlay.properties");
-pref("extensions.getanaccount.providerList", "http://broker.live.mozillamessaging.com/provider/list");
-pref("extensions.getanaccount.logUrl", "http://broker.live.mozillamessaging.com/provider/log");
-pref("extensions.getanaccount.suggestFromName", "http://broker.live.mozillamessaging.com/provider/hover/suggest");
-pref("extensions.getanaccount.checkAddress", "http://broker.live.mozillamessaging.com/provider/hover/check");
+pref("extensions.getanaccount.providerList", "https://broker-live.mozillamessaging.com/provider/list");
+pref("extensions.getanaccount.logUrl", "https://broker-live.mozillamessaging.com/provider/log");
+pref("extensions.getanaccount.suggestFromName", "https://broker-live.mozillamessaging.com/provider/hover/suggest");
+pref("extensions.getanaccount.checkAddress", "https://broker-live.mozillamessaging.com/provider/hover/check");
 
